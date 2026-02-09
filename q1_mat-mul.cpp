0a5c389ccdc958a638639c2d0edf6286a5bcf4ec
cpp <klondikecrazy1617@gmail.com>
2026-02-09 12:37:11 +0530

    Update q1_mat-mul.cpp

--- q1_mat-mul.cpp
+++ q1_mat-mul.cpp
@@ -1,41 +1,38 @@
+
 #include <iostream>
 using namespace std;
 
 int main() {
     int r1, c1, r2, c2;
-    cout << "Enter rows and columns for Matrix 1: ";
-    cin >> r1 >> c1;
-    cout << "Enter rows and columns for Matrix 2: ";
-    cin >> r2 >> c2;
+    cout << "Enter rows and cols for Matrix 1 & 2: ";
+    cin >> r1 >> c1 >> r2 >> c2;
 
-    // Validation: Columns of Matrix 1 must equal Rows of Matrix 2
+
     if (c1 != r2) {
-        cout << "Invalid Dimensions! Multiplication not possible.";
+        cout << "Invalid Dimensions!";
         return 0;
     }
 
     int A[r1][c1], B[r2][c2], C[r1][c2];
 
     cout << "Enter Matrix 1 elements:\n";
-    for(int i = 0; i < r1; i++) {
-        for(int j = 0; j < c1; j++) cin >> A[i][j];
-    }
+    for(int i = 0; i < r1 * c1; i++) cin >> *(&A[0][0] + i);
 
     cout << "Enter Matrix 2 elements:\n";
-    for(int i = 0; i < r2; i++) {
-        for(int j = 0; j < c2; j++) cin >> B[i][j];
-    }
+    for(int i = 0; i < r2 * c2; i++) cin >> *(&B[0][0] + i);
 
-    // --- Multiplication Logic (Moved from function to main) ---
+    
     for (int i = 0; i < r1; i++) {
         for (int j = 0; j < c2; j++) {
-            C[i][j] = 0; // Initialize the cell
+            
+            *(&C[0][0] + i * c2 + j) = 0;
+            
             for (int k = 0; k < c1; k++) {
-                C[i][j] += A[i][k] * B[k][j];
+                
+                *(&C[0][0] + i * c2 + j) += (*(&A[0][0] + i * c1 + k)) * (*(&B[0][0] + k * c2 + j));
             }
         }
     }
-    // ---------------------------------------------------------
 
     cout << "Product Matrix:\n";
     for (int i = 0; i < r1; i++) {
@@ -46,4 +43,3 @@ int main() {
     }
 
     return 0;
-}
