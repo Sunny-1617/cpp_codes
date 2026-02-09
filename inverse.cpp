1626c25037416dee42f47404a87423abbe6709a4
cpp <klondikecrazy1617@gmail.com>
2026-02-09 09:08:01 +0530

    inverse.cpp

--- inverse.cpp
+++ inverse.cpp
@@ -12,15 +12,15 @@ int main() {
     cout << "Enter matrix elements:\n";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) cin >> a[i][j];
-        for (int j = n; j < 2 * n; j++) a[i][j] = (i == j - n); // Fill Identity
+        for (int j = n; j < 2 * n; j++) a[i][j] = (i == j - n);
     }
     for (int i = 0; i < n; i++) {
         double pivot = a[i][i];
         if (pivot == 0) { cout << "Inverse doesn't exist"; return 0; }
 
-        for (int j = 0; j < 2 * n; j++) a[i][j] /= pivot; // Normalize row
+        for (int j = 0; j < 2 * n; j++) a[i][j] /= pivot; 
 
-        for (int k = 0; k < n; k++) { // Eliminate other rows
+        for (int k = 0; k < n; k++) { 
             if (k != i) {
                 double factor = a[k][i];
                 for (int j = 0; j < 2 * n; j++) a[k][j] -= factor * a[i][j];
